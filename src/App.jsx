cb40a51e50389d21f56545f645d8880dca444592
ErickRabago <164416841+ErickRabago@users.noreply.github.com>
2024-06-21 15:35:46 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+import Navigation from "./components/Navigation";
+import Planet from "./pages/destinations/Planet";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <div>
+                <Router>
+                    <Navigation />
+                    <Routes>
+                        <Route path="/fm-space-tourism-website" element={<Home />} />
+                        <Route path="/fm-space-tourism-website/destination" element={<Destination />}>
+                            <Route path=":postSlug" element={<Planet />} />
+                        </Route>
+                        <Route path="/fm-space-tourism-website/crew" element={<Crew />} />
+                        <Route path="/fm-space-tourism-website/tech" element={<Technology />} />
+                    </Routes>
+                </Router>
+            </div>
+
+            <footer className="h-20 text-center flex justify-center items-center">
+            </footer>
+        </>
+    );
+}
+
+export default App;
